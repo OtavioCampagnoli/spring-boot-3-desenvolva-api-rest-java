4cebd28ec840eb041a36f03e8af325b2e5af708d
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-10-01 20:38:09 -0300

    Migrations com Flyway

--- backend/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ backend/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -20,6 +21,7 @@ public class MedicoController {
 	private MedicoRepository repository;
 
 	@PostMapping
+	@Transactional
 	public void cadastrar(@RequestBody DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
 	}
