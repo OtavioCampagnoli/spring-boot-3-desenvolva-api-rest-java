f1ff6c54bcc8b223ae7512d15f09d2ad116aadd6
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-10-01 19:58:33 -0300

    Fix: Entidades JPA

--- backend/api/src/main/java/med/voll/api/medico/Medico.java
+++ backend/api/src/main/java/med/voll/api/medico/Medico.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -19,6 +22,7 @@ import med.voll.api.endereco.Endereco;
 @EqualsAndHashCode(of = "id")
 public class Medico {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
 
 	String nome;
