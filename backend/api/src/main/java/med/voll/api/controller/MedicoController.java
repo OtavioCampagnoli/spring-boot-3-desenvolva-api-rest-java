417473078e8c72f25291e80e9a6e108573dd76d0
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-10-07 08:57:42 -0300

    Produção de dados na API

--- backend/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ backend/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosDaListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -25,5 +29,10 @@ public class MedicoController {
 	@Transactional
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
+	}
+
+	@GetMapping
+	public List<DadosDaListagemMedico> listar() {
+		return repository.findAll().stream().map(DadosDaListagemMedico::new).toList();
 	}
 }
