29296b4d54a1d9726c3c6f1248b30c9c416d159d
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-10-07 09:14:22 -0300

    Paginação

--- backend/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ backend/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,8 @@ package med.voll.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +34,7 @@ public class MedicoController {
 	}
 
 	@GetMapping
-	public List<DadosDaListagemMedico> listar() {
-		return repository.findAll().stream().map(DadosDaListagemMedico::new).toList();
+	public Page<DadosDaListagemMedico> listar(Pageable paginacao) {
+		return repository.findAll(paginacao).map(DadosDaListagemMedico::new);
 	}
 }
